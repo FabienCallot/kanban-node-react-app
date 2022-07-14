d806b3fe1c44be238e32e4dc8f814620973820c4
FabienCallot <fab.callot@gmail.com>
2022-07-14 12:48:18 +0200

    create react-client and first connect with server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
