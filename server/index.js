70488e964dd7e313d547dd9cec323a40021c24c8
FabienCallot <fab.callot@gmail.com>
2022-07-14 19:33:19 +0200

    try to fix issue heroku 2

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3018
 
 /* This is telling Node to serve the files for the built React app. */
-app.use(express.static(path.resolve(__dirname, '../client/build/static')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
