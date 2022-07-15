6a4f858745a2ec3582b41a570d2fa538f6cf0edf
FabienCallot <fab.callot@gmail.com>
2022-07-15 09:44:30 +0200

    try fix connection database with heroku

--- server/app/client.js
+++ server/app/client.js
@@ -1,15 +1,7 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.PGURL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     define: {
         underscored: true
-    },
-    dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
+    }
 });
-
-
 module.exports = sequelize;
