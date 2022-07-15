afc101631a75e2c72821d5ff306acfa32ebf3490
FabienCallot <fab.callot@gmail.com>
2022-07-15 09:28:30 +0200

    modification auth sequelize

--- server/app/client.js
+++ server/app/client.js
@@ -2,6 +2,14 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.PGURL, {
     define: {
         underscored: true
-    }
+    },
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
 });
+
+
 module.exports = sequelize;
