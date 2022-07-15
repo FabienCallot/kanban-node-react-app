de6034e7c58e8b5f5ade35266eccbdc44bdb4b7a
FabienCallot <fab.callot@gmail.com>
2022-07-15 10:07:48 +0200

    try fix connection database with heroku 4

--- server/app/client.js
+++ server/app/client.js
@@ -1,13 +1,27 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    define: {
-        underscored: true
-    },
-    host: "127.0.0.1",
-    dialect: "postgres",
-    pool: {
-      max: 100,
-      min: 0,
-    },
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     define: {
+//         underscored: true
+//     },
+//     host: "127.0.0.1",
+//     dialect: "postgres",
+//     pool: {
+//       max: 100,
+//       min: 0,
+//     },
+// });
+
+const database = process.env.DATABASE_URL || 'okanban'
+const sequelize = ""
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(database)
+}
+else {
+    sequelize = new Sequelize(database, 'postgres', '', {
+        dialect: 'postgres'
+    });
+}
 module.exports = sequelize;
+
+
