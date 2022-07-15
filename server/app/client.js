7f009b6d0b53ea04317102a996a3b09641c00563
FabienCallot <fab.callot@gmail.com>
2022-07-15 11:39:16 +0200

    first try connect db heroku and server 3

--- server/app/client.js
+++ server/app/client.js
@@ -2,6 +2,13 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     define: {
         underscored: true
+    },
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
 module.exports = sequelize;
