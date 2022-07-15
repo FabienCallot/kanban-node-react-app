74e61bbd8220cfd77bd3d4805f89ecf4545165cf
FabienCallot <fab.callot@gmail.com>
2022-07-15 09:52:52 +0200

    try fix connection database with heroku 2

--- server/app/client.js
+++ server/app/client.js
@@ -2,6 +2,12 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     define: {
         underscored: true
-    }
+    },
+    host: process.env.PSQL_HOST || "localhost",
+    dialect: "postgres",
+    pool: {
+      max: 100,
+      min: 0,
+    },
 });
 module.exports = sequelize;
