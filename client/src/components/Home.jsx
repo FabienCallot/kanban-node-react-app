db8912c688bb71eebc0a9b4f220d211aab01f689
FabienCallot <fab.callot@gmail.com>
2022-07-25 12:07:30 +0200

    refacto home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,11 +4,10 @@ import List from './List';
 const Home = () => {
   return (
     <div className="home-container p-8">
-      <div className=" p-4 ">
-        <h2 className="text-3xl mt-20 mb-10">RoadMap</h2>
-        <div className="home-lists">
-          <List />
-        </div>
+      <h2 className="text-3xl mt-20 mb-10">RoadMap</h2>
+      <div className="home-lists flex">
+        <List />
+        <List />
       </div>
     </div>
   );
