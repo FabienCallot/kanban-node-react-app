0803670c9a6be0266cfd851323912a93cfbc0331
FabienCallot <fab.callot@gmail.com>
2022-07-25 17:39:04 +0200

    data linked for tasks, add request getAllCardsByListId and refacto

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { CgProfile } from 'react-icons/cg';
 
 const Header = ({ className }) => {
-  console.log(className);
   return (
     <div
       className={`header flex justify-between fixed left-0 right-0 overflow-x-scroll bg-[#262626] p-4
