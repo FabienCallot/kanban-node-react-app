1a14805ad0f8aff1a053d2717a736267db6af0ac
FabienCallot <fab.callot@gmail.com>
2022-07-28 11:47:27 +0200

    fix favico and refacto

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 
-
 const express = require('express');
 const router = require('./app/routers');
-const app = express()
-const port = process.env.PORT || 3018
+const app = express();
+const port = process.env.PORT || 3018;
 
 const path = require('path');
 const cors = require('cors');
@@ -12,30 +11,21 @@ const bodySanitizer = require('./app/middlewares/bodySanitizer');
 const multer = require('multer');
 const bodyParser = multer();
 
-
-
-
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(bodySanitizer);
 
-app.use( bodyParser.none() );
+app.use(bodyParser.none());
 
 /* This is telling Node to serve the files for the built React app. */
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from server!' });
 });
-
-// All other GET requests not handled before will return our React app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// });
 
 app.use(router);
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
-
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
