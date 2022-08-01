ba818be3076379c051fa60fda735d520f8dc01f5
FabienCallot <fab.callot@gmail.com>
2022-08-01 11:50:06 +0200

    new user table in dataBase with list association

--- server/data/import_data.sql
+++ server/data/import_data.sql
@@ -1,4 +1,6 @@
 BEGIN;
+INSERT INTO "user" ("id", "email", "first_name", "last_name", "password") VALUES
+(1, 'fab.callot@gmail.com', 'Fab', 'Call', 'fab12345');
 
 INSERT INTO "tag" ("id", "name", "color") VALUES 
 (1, 'urgent', '#C340A1'),
@@ -6,10 +8,10 @@ INSERT INTO "tag" ("id", "name", "color") VALUES
 (3, 'bug', '#DA3A3A'),
 (4, 'process', '#3F88E4');
 
-INSERT INTO "list" ("id", "name") VALUES
-(1, 'Backlog'),
-(2, 'In progress'),
-(3, 'Done');
+INSERT INTO "list" ("id", "name", "user_id") VALUES
+(1, 'Backlog', 1),
+(2, 'In progress', 1),
+(3, 'Done', 1);
 
 INSERT INTO "card" ("id", "description", "list_id", "color") VALUES
 (1, 'Chose à faire', 1, '#6b7280'),
@@ -25,6 +27,7 @@ INSERT INTO "card_has_tag" ("card_id", "tag_id") VALUES
 (3,2),
 (4,2);
 
+SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
 SELECT setval('tag_id_seq', (SELECT MAX(id) from "tag"));
 SELECT setval('list_id_seq', (SELECT MAX(id) from "list"));
 SELECT setval('card_id_seq', (SELECT MAX(id) from "card"));
