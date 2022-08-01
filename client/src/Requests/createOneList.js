dd9fa3bcd61c7f00d34605919572ff039bde0e9b
FabienCallot <fab.callot@gmail.com>
2022-08-01 16:47:19 +0200

    todo user_id

--- client/src/Requests/createOneList.js
+++ client/src/Requests/createOneList.js
@@ -1,8 +1,9 @@
-
 export async function createOneList(event, name) {
   event.preventDefault();
   const data = new FormData();
   data.append('name', name);
+  //TODO: 1 for the moment
+  data.append('user_id', 1);
 
   const response = await fetch(
     'https://kanban-node-react-app.herokuapp.com/lists',
