00b9ff5a6ab0131a3fc3ea93e1ae09c740270fd8
FabienCallot <fab.callot@gmail.com>
2022-08-02 16:15:32 +0200

    fix issue fetch witch data form

--- server/index.js
+++ server/index.js
@@ -4,24 +4,14 @@ const express = require('express');
 const router = require('./app/routers');
 const app = express();
 const port = process.env.PORT || 3018;
-
 const path = require('path');
 const cors = require('cors');
 const bodySanitizer = require('./app/middlewares/bodySanitizer');
-const multer = require('multer');
-const bodyParser = multer();
+/* This is telling Node to serve the files for the built React app. */
+//FIXME: try without this statc serve
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use(cors({ origin: true }));
-
-// app.use(express.urlencoded({ extended: true }));
-
-/* This is telling Node to serve the files for the built React app. */
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
-});
-
 /* Parsing the body of the request. */
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
