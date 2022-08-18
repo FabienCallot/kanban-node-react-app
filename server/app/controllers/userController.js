5a9e2b60552d14e6f864dae7399dfa67ee82969d
FabienCallot <fab.callot@gmail.com>
2022-08-18 10:11:44 +0200

    modification listController for check if list already exist with his name

--- server/app/controllers/userController.js
+++ server/app/controllers/userController.js
@@ -49,7 +49,6 @@ const userController = {
 
   /* We are trying to recover the user who has a given email. */
   async loginAction(req, res) {
-    console.log(req.body);
     try {
       const user = await User.findOne({
         where: {
